0dd85abffc73ebf3b0af19b13d50a32e76bdcac1
Jeongseok Kim <suku19980923@gmail.com>
2022-06-11 16:38:31 +0900

    ComputerDepartmentPage-jSP-Team5
    로그인 기능 및 user DTO DAO

---
+++ src/main/java/user/UserDTO.java
@@ -0,0 +1,49 @@
+package user;
+
+public class UserDTO {
+
+		private String userID;
+		private String userPassword;
+		private String userName;
+		private String userEmail;
+		private String userGender;
+		private String userAuth;
+		
+		public String getUserID() {
+			return userID;
+		}
+		public void setUserID(String userID) {
+			this.userID = userID;
+		}
+		public String getUserPassword() {
+			return userPassword;
+		}
+		public void setUserPassword(String userPassword) {
+			this.userPassword = userPassword;
+		}
+		public String getUserName() {
+			return userName;
+		}
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+		public String getUserEmail() {
+			return userEmail;
+		}
+		public void setUserEmail(String userEmail) {
+			this.userEmail = userEmail;
+		}
+		public String getUserGender() {
+			return userGender;
+		}
+		public void setUserGender(String userGender) {
+			this.userGender = userGender;
+		}
+		public String getUserAuth() {
+			return userAuth;
+		}
+		public void setUserAuth(String userAuth) {
+			this.userAuth = userAuth;
+		}
+		
+}
